7fd701722c518a9fb68fb849953d33138dcfbec1
Joseph Garner <joseph.peter.garner@gmail.com>
2018-03-24 17:49:03 +1100

    Expanding on dbquery

--- dbquery.java
+++ dbquery.java
@@ -1,10 +1,26 @@
 import javax.sound.midi.SysexMessage;
 
 public class dbquery{
+    private static File inFile = null;
     private static int pagesize = 32768;
+    private static ArrayList<Page> heap = new ArrayList<Page>();
+    private static ExecutionTimer timer = new ExecutionTimer();
 
     private static int convert(String s) throws NumberFormatException{
         return Integer.parseInt(s);
+    }
+
+    private static void readArray(){
+        BufferedReader reader = new BufferedReader(new FileReader(inFile));
+        while ((line = reader.readLine()) != null) {
+            
+        }
+    }
+
+    private static void searchResulkt(){
+        System.out.printf("## stdout ##\n");
+        System.out.printf("%-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s\n","BN_NAME","BN_STATUS","BN_REG_DT","BN_CANCEL_DT","BN_RENEW_DT","BN_STATE_NUM","BN_STATE_OF_REG","BN_ABN");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
     }
 
     //MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -18,7 +34,6 @@ public class dbquery{
             }
         }
         
-        System.out.println(args[0]);
-        System.out.println(pagesize);
+        infile = new File(args[0]);
     }
 }
